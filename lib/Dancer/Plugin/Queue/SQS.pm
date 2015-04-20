84e956bb55c222d440a613bf8a12dfd85d045fa4
Colin Ewen <colin@draecas.com>
2015-04-20 17:52:46 -0400

    Update Abstract, implement queue

--- lib/Dancer/Plugin/Queue/SQS.pm
+++ lib/Dancer/Plugin/Queue/SQS.pm
@@ -6,6 +6,7 @@ use warnings;
 use Amazon::SQS::Simple;
 
 use Moo;
+use namespace::autoclean;
 with 'Dancer::Plugin::Queue::Role::Queue';
 
 =head1 NAME
@@ -20,9 +21,55 @@ Version 0.0.1
 
 our $VERSION = '0.0.1';
 
+my $settings = plugin_setting;
 
-sub add_msg {}
+has secret_key => (
+  is => 'ro',
+  required => 1
+);
 
-sub get_msg {}
+has access_key => (
+  is => 'ro',
+  required => 1
+);
 
-sub remove_msg {}
+has queue_name => (
+  is => 'ro',
+  required => 1
+);
+
+has queue => (
+  is => 'lazy'
+);
+
+has sqs => (
+  is => 'lazy'
+);
+
+sub _build_queue {
+  my ($self) = @_;
+  return $self->sqs->CreateQueue($self->queue_name);
+}
+
+sub _build_sqs {
+  my ($self) = @_;
+  return new Amazon::SQS::Simple($self->access_key, $self->secret_key);
+}
+
+sub add_msg {
+  my ($self, $data) = @_;
+  return $self->queue->SendMessage($data);
+}
+
+sub get_msg {
+  my ($self) = @_;
+  my $msg = $self->queue->ReceiveMessage();
+  return ( $msg, $msg->MessageBody() );
+}
+
+sub remove_msg {
+  my ($self, $msg) = @_;
+  $self->queue->DeleteMessage($msg);
+}
+
+1;
